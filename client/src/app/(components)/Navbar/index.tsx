d53e3ae79b37e8718e8b3a31c454bb880f36ccec
karnowsky1 <kestime9015@bths.edu>
2024-08-04 20:31:05 -0400

    implemented sidebar sections and hamburger  functionality

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,15 +1,30 @@
 "use client";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
 import { Bell, Menu, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch()
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  )
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+  }
+
+  const sidebarClassNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* LEFT SIDE */}
       <div className="flex justify-between items-center gap-5">
         <button 
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" 
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" /> 
         </button>
